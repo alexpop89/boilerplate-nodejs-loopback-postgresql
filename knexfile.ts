e2ed0aab3fe432cafad242c65f1fbdafb93beeb0
Alex Pop <alexandru.pop@sfappworks.com>
2023-10-26 01:12:48 +0300

    Add live reload. Cleanup

--- knexfile.ts
+++ knexfile.ts
@@ -1,4 +1,18 @@
 module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+    },
+
+    migrations: {
+      directory: './src/migrations',
+    },
+  },
   client: 'pg',
   connection: {
     host: process.env.DB_HOST,
@@ -7,6 +21,7 @@ module.exports = {
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
   },
+
   migrations: {
     directory: './migrations',
   },
--- run-tests.sh
+++ run-tests.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-source .env
+export $(cat .env)
 export DB_NAME="test-database"
-export DB_USER="test-username"
-export DB_PASSWORD="test-password"
 export DB_PORT="5433"
 
 docker-compose up postgres_test -d
 sleep 3
-DB_HOST=localhost DB_USER=test-username DB_PASSWORD=test-password DB_NAME=test-database DB_PORT=5433 yarn run migrate
+yarn run build:dev && yarn run migrate
 lb-nyc lb-mocha --timeout 5000 --allow-console-logs \"dist/__tests__\"
 EXIT_CODE=$?
 
